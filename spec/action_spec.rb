13af2c595db6be6e94a87a3b3363f536e92a68e1
nesk27 <nikitaselytin@yandex.ru>
2018-10-28 17:32:37 +0700

    Add new test for actions - films_and_vines

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -11,6 +11,7 @@ describe Actions do
 		expect(@valera.alcomana).to eq 0
 		expect(@valera.money).to eq 400
 		expect(@valera.tired).to eq 70
+		expect(@valera.health).to eq 100
 
 	end
 
@@ -21,5 +22,17 @@ describe Actions do
 		expect(@valera.alcomana).to eq 0
 		expect(@valera.money).to eq 300
 		expect(@valera.tired).to eq 10
+		expect(@valera.health).to eq 100
 	end
+
+	it "should change value by films_and_vines" do
+		@valera = Valera.new
+		@actions.films_and_vines(@valera)
+		expect(@valera.happy).to eq 1
+		expect(@valera.alcomana).to eq 30
+		expect(@valera.money).to eq 280
+		expect(@valera.tired).to eq 10
+		expect(@valera.health).to eq 95
+	end
+
 end
