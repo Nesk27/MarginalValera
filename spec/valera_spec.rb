f82198113aa83e49e7c7062cc35a64b6c7185330
nesk27 <nikitaselytin@yandex.ru>
2018-10-28 16:00:22 +0700

    Add tests for new attribute in class Valera

--- spec/valera_spec.rb
+++ spec/valera_spec.rb
@@ -4,9 +4,27 @@ require_relative '../valera.rb'
 describe Valera do 
 	before {@valera = Valera.new}
 
-it "should health eq 100" do
-	expect(@valera.health).to eq 100
-end
+	it "should health eq 100 on start" do
+		expect(@valera.health).to eq 100
+	end
+
+	it "should alcomana eq 0 on start"
+		expect(@valera.alcomana).to eq 0
+	end
+
+	it "should happy eq 0 on start"
+		expect(@valera.happy).to eq 0
+	end
+
+	it "should tired eq 0 on start"
+		expect(@valera.tired).to eq 0
+	end
+
+	it "should money eq 300 on start"
+		expect(@valera.money).to eq 300
+	end
+
+
 
 end
 
