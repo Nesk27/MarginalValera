4523069c21d03cba4c7a9c3773ac480e9483d14f
nesk27 <nikitaselytin@yandex.ru>
2018-10-28 16:01:34 +0700

    Add new attribute in class Valera

--- valera.rb
+++ valera.rb
@@ -1,9 +1,12 @@
 class Valera
 
-	attr_accessor :health
+	attr_accessor :health, :alcomana, :happy, :tired, :money
 
 	def initialize
 		@health = 100
-		return @health
+		@alcomana = 0
+		@happy = 0
+		@tired = 0
+		@money = 300
 	end
 end
