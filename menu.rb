d05b26e2c74ec8717f58a95dec81945e06ec6aad
nesk27 <nikitaselytin@yandex.ru>
2018-10-29 20:29:18 +0700

    Create first method in new calss Menu

--- menu.rb
+++ menu.rb
@@ -2,9 +2,11 @@ require_relative './actions.rb'
 
 class Menu
 
-	def print_stats
-		puts ""
-			
+	def print_stats(valera)
+		puts 'Параметры Валеры:'
+		puts "Здоровье: #{valera.health} \t Алкомана: #{valera.alcomana}"
+		puts "Счастье: #{valera.happy} \t Усталость: #{valera.tired}"
+		puts "Деньги: #{valera.money}$"	
 	end	
 
 end
