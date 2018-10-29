554ff9389cb933b211c60e7a09e585b0fba3e87c
nesk27 <nikitaselytin@yandex.ru>
2018-10-29 20:34:10 +0700

    Create new method in new calss Menu

--- menu.rb
+++ menu.rb
@@ -6,7 +6,21 @@ class Menu
 		puts 'Параметры Валеры:'
 		puts "Здоровье: #{valera.health} \t Алкомана: #{valera.alcomana}"
 		puts "Счастье: #{valera.happy} \t Усталость: #{valera.tired}"
-		puts "Деньги: #{valera.money}$"	
+		puts "Деньги: #{valera.money}$\n\n"	
 	end	
 
+	def print_actions
+		puts "1 - Пойти на работу"
+		puts "2 - Созерцать природу"
+		puts "3 - Пить вино и смотреть сериал"
+		puts "4 - Сходить в бар"
+		puts "5 - Выпить с маргинальными личностями"
+		puts "6 - Петь в метро"
+		puts "7 - Спать"
+	end
 end
+
+menu = Menu.new
+valera = Valera.new
+menu.print_stats(valera)
+menu.print_actions
